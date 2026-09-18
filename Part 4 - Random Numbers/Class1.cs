b3a0faaa4c567c72845cba77e7136fb19f59f9e4
362568016 <362568016@2260I-L29-025>
2026-09-18 13:18:43 -0400

    Add project files.

---
+++ Part 4 - Random Numbers/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_4___Random_Numbers
+{
+    internal class Class1
+    {
+        public class Die
+        {
+            private int _sides;
+            private int _roll;
+            private Random _generator;
+            private ConsoleColor _color;
+
+            public Die()
+            {
+                _generator = new Random();
+                _sides = 6;
+                _roll = _generator.Next(1, _sides + 1);
+                _color = ConsoleColor.Gray;
+            }
+            
+
+            //properties
+            public int Roll
+            {
+                get { return _roll; }
+                //set { _roll = value; }
+            }
+
+            public ConsoleColor Color
+            {
+                get { return _color; }
+                set { _color = value; }
+            }
+
+            public override string ToString()
+            {
+                return _roll.ToString();
+            }
+
+            public void RollDie()
+            {
+                _roll = _generator.Next(1, _sides + 1);
+            }
+        }
+    }
+}
---
+++ Part 4 - Random Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using Part_4___Random_Numbers;
+int minNumber, maxNumber, number1, number2, number3, number4, number5;
+Random generator = new Random();
+
+
+Console.WriteLine("hey, type in a number");
+minNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Great! Now type in a number greater than the previous");
+maxNumber  = Convert.ToInt32(Console.ReadLine());
+number1 = generator.Next(minNumber, maxNumber);
+number2 = generator.Next(minNumber, maxNumber);
+number3 = generator.Next(minNumber, maxNumber);
+number4 = generator.Next(minNumber, maxNumber);
+number5 = generator.Next(minNumber, maxNumber);
+Console.WriteLine($"Numbers are {number1} {number2} {number3} {number4} {number5}");
+
+
+Die die1 = new Die();
+Console.WriteLine(die1);
+die1.RollDie();
+
+Die die2 = new Die();
+Console.WriteLine(die1);
+die2.RollDie();
