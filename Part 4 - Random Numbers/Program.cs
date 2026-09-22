5e817de1d63c8f0dc26fec55d8928c7a31b5e262
362568016 <362568016@2260I-L29-025>
2026-09-22 12:46:02 -0400

    final thing

--- Part 4 - Random Numbers/Program.cs
+++ Part 4 - Random Numbers/Program.cs
@@ -1,12 +1,12 @@
-﻿using Part_4___Random_Numbers;
-int minNumber, maxNumber, number1, number2, number3, number4, number5;
+﻿int minNumber, maxNumber, number1, number2, number3, number4, number5, dieNumber1, dieNumber2, totalNumber; 
+double decimalNumber1, decimalNumber2, decimalNumber3;
 Random generator = new Random();
 
 
 Console.WriteLine("hey, type in a number");
 minNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Great! Now type in a number greater than the previous");
-maxNumber  = Convert.ToInt32(Console.ReadLine());
+maxNumber = Convert.ToInt32(Console.ReadLine());
 number1 = generator.Next(minNumber, maxNumber);
 number2 = generator.Next(minNumber, maxNumber);
 number3 = generator.Next(minNumber, maxNumber);
@@ -15,10 +15,16 @@ number5 = generator.Next(minNumber, maxNumber);
 Console.WriteLine($"Numbers are {number1} {number2} {number3} {number4} {number5}");
 
 
-Die die1 = new Die();
-Console.WriteLine(die1);
-die1.RollDie();
+dieNumber1 = generator.Next(1, 7);
+dieNumber2 = generator.Next(1, 7);
+totalNumber = (dieNumber1 + dieNumber2);
+Console.WriteLine($"Your two dice numbers are {dieNumber1} and {dieNumber2}.");
+Console.WriteLine($"The Total is {totalNumber}!");
 
-Die die2 = new Die();
-Console.WriteLine(die1);
-die2.RollDie();
+
+//decimalNumber1 = generator.NextDouble() + Convert.ToInt32(Console.ReadLine());
+//decimalNumber2 = generator.NextDouble() + Convert.ToInt32(Console.ReadLine());
+//decimalNumber3 = generator.NextDouble() + Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(decimalNumber1, decimalNumber2, decimalNumber3);
+
+//i give up
